1653e88f29c8d49214177d61528f372c41818043
Khang Tran <khangtrann8198@gmail.com>
2024-10-19 13:47:06 +0700

    feat(post-entry): implement dummy UI

---
+++ src/app/features/post-list/post-entry/post-entry.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SvgIconComponent } from 'angular-svg-icon';
+
+@Component({
+  selector: 'app-post-entry',
+  standalone: true,
+  imports: [SvgIconComponent],
+  templateUrl: './post-entry.component.html',
+  styleUrl: './post-entry.component.scss'
+})
+export class PostEntryComponent {
+
+}
--- src/app/features/post-list/post-list.component.ts
+++ src/app/features/post-list/post-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PostEntryComponent } from './post-entry/post-entry.component';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [PostEntryComponent],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss'
 })
