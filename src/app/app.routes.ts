3807ef3cd9b3014092395012f3fe958d63cdc6f5
Khang Tran <khangtrann8198@gmail.com>
2024-10-19 13:12:06 +0700

    feat(): init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: async () => (await import('./features/post-list/post-list.component')).PostListComponent,
+  }
+];
