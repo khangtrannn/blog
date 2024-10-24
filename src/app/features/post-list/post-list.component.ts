9fb1c9364b39097c0992faa86309449db5a173c2
Khang Tran <khangtrann8198@gmail.com>
2024-10-24 20:31:52 +0700

    feat(): implement add new document link

--- src/app/features/post-list/post-list.component.ts
+++ src/app/features/post-list/post-list.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { PostEntryComponent } from './post-entry/post-entry.component';
 import { PostService } from '../../core/post.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { RouterLink } from '@angular/router';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [PostEntryComponent],
+  imports: [PostEntryComponent, RouterLink, SvgIconComponent],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss',
 })
